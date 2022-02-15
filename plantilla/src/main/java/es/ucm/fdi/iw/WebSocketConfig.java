a2e91d52158332b383373c67000fb7e2aed284ad
erosguerrero <erosjgs@gmail.com>
2022-02-15 14:34:32 +0100

    Revert "prueba"
    This reverts commit c8837212b1e94cffd833366ba4da88775eac3a4e.

--- plantilla/src/main/java/es/ucm/fdi/iw/WebSocketConfig.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/WebSocketConfig.java
@@ -17,7 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
         registration.setSendTimeLimit(15 * 1000).setSendBufferSizeLimit(512 * 1024);
     }
-    //Para ver si funciona
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
