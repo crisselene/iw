6627e57d2bd16ec8bbd6dd76e302be9d996acbda
AlPasc04 <alpasc04@ucm.es>
2022-02-22 19:29:03 +0100

    Funcionalidad dinamica y estilo de ver categorias, y platos por categoria
    Utilizando Thymeleaf y metiendo la primera clase de modelo (categorias), se introducen datos de prueba al modelos y se cargan dinamicamente con thymeleaf (para mostrar las categorias). tambien se le da estilo para que queden todas las imagenes iguales de tamaño. Se corrige tambien el estilo y funcionalidad de la vista para ver los platos de cada categoria.

---
+++ plantilla/src/main/java/es/ucm/fdi/iw/model/Categorias.java
@@ -0,0 +1,24 @@
+package es.ucm.fdi.iw.model;
+
+import java.util.ArrayList;
+
+import antlr.collections.List;
+
+public class Categorias {
+    public String cat = "soy una categoria de un objeto";
+    public ArrayList<String> lista; 
+
+    public Categorias()
+    {
+        lista = new ArrayList<String>();
+        lista.add("Entrantes");
+        lista.add("Carnes");
+        lista.add("Pasta");
+        lista.add("Burguers");
+        lista.add("Pizzas");
+        lista.add("Tacos");
+        lista.add("Ensaladas");
+        lista.add("Postres");
+    }
+    
+}
