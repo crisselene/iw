20dff2c824c220cfda9ac423850674fa5f5665bd
Samu <samuelete001@gmail.com>
2022-03-08 14:18:03 +0100

    para que funcione

--- src/main/java/es/ucm/fdi/iw/model/SA/SACategoriaImp.java
+++ src/main/java/es/ucm/fdi/iw/model/SA/SACategoriaImp.java
@@ -1,19 +1,26 @@
 package es.ucm.fdi.iw.model.SA;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-import es.ucm.fdi.iw.model.Categorias;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import es.ucm.fdi.iw.model.Categorias;
+import lombok.Data;
+@Data
 public class SACategoriaImp implements SACategoria{
 
     @Override
-    public List<Categorias> listarCategorias() {
-        EntityManagerFactory emf;
-        EntityManager em = emf.createEntityManager();
-        return null;
+    public List<Categorias> listarCategorias(EntityManager em) {
+        List<Categorias> categorias;
+       
+        categorias = em.createQuery("SELECT * FROM Categorias").getResultList();
+        return categorias;
     }
     
 }
