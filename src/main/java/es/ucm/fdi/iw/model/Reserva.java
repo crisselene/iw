680162cfa803edae5c352e635f4de1692d4036d2
Samu <samuelete001@gmail.com>
2022-03-22 13:22:02 +0100

    Comienzo de ajax

--- src/main/java/es/ucm/fdi/iw/model/Reserva.java
+++ src/main/java/es/ucm/fdi/iw/model/Reserva.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
@@ -14,6 +16,10 @@ import lombok.Data;
 
 @Entity
 @Data
+@NamedQueries({
+    @NamedQuery(name = "es.ucm.fdi.iw.model.Reserva.findById", query = "select obj from Reserva obj where  :id = obj.id"),
+    @NamedQuery(name = "es.ucm.fdi.iw.model.Reserva.findByFecha", query = "select obj from Reserva obj where  :fecha+1 >= obj.fecha and :fecha <= obj.fecha")
+})
 @AllArgsConstructor
 public class Reserva {
     @Id
@@ -22,7 +28,6 @@ public class Reserva {
 	private long id;
     private LocalDateTime fecha;
     private int personas;
-    //private long id_cliente;
     private boolean activo;
 
     public Reserva()
@@ -30,4 +35,12 @@ public class Reserva {
     
     @ManyToOne
     private User cliente; //Quien hizo la reserva
+
+    @Data
+    @AllArgsConstructor
+    public class Transfer{
+        String fecha;
+    }
+
+
 }
