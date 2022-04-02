c5139f8d2c29062a231540abe7572ead34a05760
crisselene <criisalways@gmail.com>
2022-04-02 14:46:08 +0200

    aceptar funcionando
    aceptar funciona pero falta cambiar algunos detalles como los botones de la tabla pedidos en curso. falta botón rechazar

--- src/main/resources/static/js/pedidosEmpleado.js
+++ src/main/resources/static/js/pedidosEmpleado.js
@@ -5,36 +5,42 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
     document.querySelectorAll(".divCambiar").forEach(d => {
-        const id = d.dataset.id;
+        //OJO como hemos puesto th:attr="data-id=${ped.id}" en el html
+        //de divCambiar entonces para coger el dato de ped.id, que es
+        //el id del pedido que queremos manejar, entonces hay que 
+        //poner dataset:
+        const id = d.dataset.id; 
         const div = d;
         const enCurso = document.querySelector(".pedEnCurso");
+        //********************BOTON ACEPTAR************************
+        //lo que hace es que al clicar aceptar cambiamos el valor
+        //de enCurso de 0 a 1, y lo cambiamos de tabla en la vista
         d.querySelector(".aceptar").addEventListener("click", e =>{
             console.log("Aceptando elemento id", id)
-            if(sePuedeCambiar){
-                console.log("SE PUEDE CAMBIAR")
-                enCurso.append(div);
-            }
             
-
+            //Realizamos el cambio con el controlador para que 
+            //se ponga en curso =1
+            let params = {"idPed" : id};
+            console.log(params)
+            go(config.rootUrl + "/aceptarPed" , 'POST', params)
+            .then(d =>{
+                console.log("en curso: ", d['encurso'])
+                //si enCurso=true entonces podemos cambiarlo a la tabla
+                //de pedidos en curso
+                if(d['encurso']== true){
+                    console.log("SE PUEDE CAMBIAR")
+                    enCurso.append(div);
+                }else{
+                    console.log("NOOOOO")
+                    cambiar=false;       
+                }
+            })                    
         })
+        //********************FIN BOTON ACEPTAR******************/
         d.querySelector(".rechazar").addEventListener("click", e =>{
             console.log("Rechazando elemento id", id)
         })     
     })
 })
 
-function sePuedeCambiar(){
-    let params = {"idPed" : document.querySelector("aceptar").value};
-    go(config.rootUrl + "/aceptarPed" , 'POST', params)
-    .then(d =>{
-        console.log("todo ok")
-        if(d['encurso']== true){
-            console.log("se puede cambiar");
-            return true;
-        }else{
-            return false;
-            console.log("NOOOOO")
-        }
-       
-    })
-}
+
