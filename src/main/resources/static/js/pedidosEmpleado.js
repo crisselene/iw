6c2f2a45e12acf5c7021fba846c2a2685dd3d449
erosguerrero <erosjgs@gmail.com>
2022-05-01 12:52:59 +0200

    La biblia de pedidoEmpleado
    Disfruten

--- src/main/resources/static/js/pedidosEmpleado.js
+++ src/main/resources/static/js/pedidosEmpleado.js
@@ -338,8 +338,9 @@ function loadAccordion(e){
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel /
     this.classList.toggle("active");
-
+    
     / Toggle between hiding and showing the active panel */
+
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
